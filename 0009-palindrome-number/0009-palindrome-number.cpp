84191247f242652a95430a8ace8198087f28cf5e
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-09-18 16:15:41 +0900

    Time: 4 ms (82.35%), Space: 6.5 MB (7.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,18 +3,16 @@ public:
     bool isPalindrome(int x) {
         if(x < 0 || (x != 0 && x % 10 == 0)) return false;
 
-        long temp = x;
-        long reverse = 0;
-        while(temp > 0)
+        // 굳이 전체를 다 뒤집지 않아도, 절반만 뒤집어도 문제 없다. 오히려 절반만 뒤집는게, 아직 안 뒤집힌 부분하고, 뒤집힌 부분ㄴ하고 비교하면 바로 대칭여부를 알 수 있다
+        int reverse = 0;
+        while(x > reverse)
         {
-            reverse += temp % 10;
             reverse *= 10;
-            temp = temp / 10;
+            reverse += x % 10;
+            
+            x = x / 10;
         }
 
-        reverse  = reverse /10;
-        if(reverse == x) return true;
-
-        return false;
+        return (reverse == x || reverse / 10 == x );
     }
 };
