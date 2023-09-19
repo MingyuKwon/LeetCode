c5438eadbd2bce98b1a0c479f29b43d80289b5d0
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-09-19 16:39:10 +0900

    Time: 9 ms (19.64%), Space: 15.3 MB (10.72%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -22,43 +22,15 @@ public:
             return list1;
         }
 
-        ListNode* result = list1;
-
-        if(result->val <= list2->val)
+        if(list1->val < list2->val)
         {
-            list1 = list1->next;
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         }else
         {
-            result = list2;
-            list2 = list2->next;
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
 
-        ListNode* temp = result;
-
-        while(list1 != NULL && list2 != NULL)
-        {
-            if(list1->val < list2->val){
-                temp->next = list1;
-                list1 = list1->next;
-            }else
-            {
-                temp->next = list2;
-                list2 = list2->next;
-            }
-
-            temp = temp->next;
-        }
-
-        if(list1 == NULL)
-        {
-            temp->next = list2;
-            list2 = list2->next;
-        }else if(list2 == NULL)
-        {
-            temp->next = list1;
-            list1 = list1->next;
-        }
-        
-        return result;
     }
 };
