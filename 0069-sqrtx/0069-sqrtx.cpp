fb83276a997011c9ad4ac27f5c913abeab871072
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-09-21 18:56:43 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (6.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        for(long i=1; i<=x; i++)
-        {
-            if(x == i*i)
-            {
-                return i;
-            }else if(i*i > x )
-            {
-                return i-1;
-            }
-        }
-
         int min = 0;
         int max = x;
         int mid = 0;
