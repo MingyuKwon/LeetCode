77b7c7a40aa5a8f1e2835269a6b27789db3e0f7e
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-09-26 11:04:32 +0900

    Time: 19 ms (35.32%), Space: 17.3 MB (24.00%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> hashMap;
-
-        for(int I : nums)
+        int ans = 0;
+        for(int a : nums)
         {
-            if(hashMap.find(I) == hashMap.end())
-            {   
-                hashMap[I] = 1;
-            }else
-            {
-                hashMap[I] = 2;
-            }
-            
+            ans = ans ^ a;
         }
 
-        for(auto a : hashMap)
-        {
-            if(a.second == 1) return a.first;
-        }
-
-        return 0;
+        return ans;
     }
 };
+
+//METHOD 3 : USING BITWISE XOR OPERATOR (USING CONSTANT SPACE )
+// To use this approach you first need to understand about Bitwise XOR operator.
+// Most of us who have a background in physics ( highschool level ) , are aware of the LOGIC GATES.
+// One of such gates is the XOR Gate :
+// According to this gate , the output is true , only if both the inputs are of opposite kind .
+// That is ,
+// A B Y
+// 0 0 0
+// 0 1 1
+// 1 0 1
+// 1 1 0
+
+// We apply the extended version of this gate in our bitwise XOR operator.
+// If we do "a^b" , it means that we are applying the XOR gate on the 2 numbers in a bitwise fashion ( on each of the corresponding bits of the numbers).
+// Similarly , if we observe ,
+
+// A^A=0
+// A^B^A=B
+// (A^A^B) = (B^A^A) = (A^B^A) = B This shows that position doesn't matter.
+// Similarly , if we see , a^a^a......... (even times)=0 and a^a^a........(odd times)=a
