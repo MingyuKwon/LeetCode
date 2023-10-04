4330fbb8581f313faafad72688fc0c9b99e910d5
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-04 12:48:35 +0900

    Time: 28 ms (35.28%), Space: 31.1 MB (18.93%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -14,31 +14,28 @@ public:
     int countNodes(TreeNode* root) {
         if(!root) return 0;
 
-        TreeNode* temp = root;
+        int leftHeight = countHeight(root->left);
+        int rightHeight = countHeight(root->right);
 
-        int leftHeight = 0;
-        int rightHeight = 0;
-
-        while(temp)
+        if(leftHeight == rightHeight)
         {
-            temp = temp->left;
-            leftHeight++;
-        }
-
-        temp = root;
-        while(temp)
+            return countNodes(root->right) + pow(2,leftHeight);
+        }else
         {
-            temp = temp->right;
-            rightHeight++;
+            return  pow(2,rightHeight) + countNodes(root->left);
         }
-
-        if(leftHeight == rightHeight) return pow(2,leftHeight) - 1;
         
-        return countNodes(root->right) + countNodes(root->left) + 1;
+    }
 
+    int countHeight(TreeNode* root)
+    {
+        int n = 0;
+        while(root)
+        {
+            root = root->left;
+            n++;
+        }
+
+        return n;
     }
 };
-
-//핵심은 완전 이진 트리에서는 좌우 높이를 계산해서 같다면 그 즉시 노드의 개수를 알 수 있다는 것
-// 그리고 완전 이진 트리라고 확정이 나면 좌우, 한쪽은 반드시 좌우 높이가 동일한 경우가 생긴다
-// 따라서 좌우 모두 재귀를 돌려도 한쪽은 재귀에 안들어가고 바로 값을 반환하기에 높이만큼인 lonN 만큼만 호출되고 끝날 수 있다
