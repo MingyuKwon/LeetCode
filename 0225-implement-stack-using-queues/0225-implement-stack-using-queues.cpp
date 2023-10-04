062a7ca6fb4be6f6f0876fbc2aa7c045a83a0423
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-04 14:52:32 +0900

    Time: 3 ms (39.51%), Space: 7.3 MB (5.83%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,48 @@
+class MyStack {
+public:
+    MyStack() {
+        
+    }
+
+    //실제 스택을 구현하는 것에 집착하지 말고, 스택의 특징인 FIFO를 구현하는 것에 중점을 두면 자연스럽게 구현할 수 있다
+    // 따라서 마지막으로 들어온 원소를 가장 큐의 앞부분으로 보내주는 것만 구현 한다면 성공적이다
+    
+    void push(int x) {
+
+        Queue2.push(x);
+        while(!Queue1.empty())
+        {
+            Queue2.push(Queue1.front());
+            Queue1.pop();
+        }
+
+        swap(Queue2, Queue1);
+    }
+    
+    int pop() {
+        int val = Queue1.front();
+        Queue1.pop();
+        return val;
+    }
+    
+    int top() {
+        return Queue1.front();
+    }
+    
+    bool empty() {
+        return Queue1.empty();
+    }
+
+private:
+    queue<int> Queue1;
+    queue<int> Queue2;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
