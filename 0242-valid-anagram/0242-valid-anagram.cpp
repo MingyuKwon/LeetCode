b57b218781e6e1aa3670c5c608502801844689b8
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-05 17:49:24 +0900

    Time: 10 ms (54.68%), Space: 7.7 MB (26.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -7,25 +7,26 @@ public:
 
         for(char c : s)
         {
-            if(hashMap.find(c) == hashMap.end())
-            {
-                hashMap[c] = 1;
-            }else
-            {
-                hashMap[c]++;
-            }
+            hashMap[c]++;
         }
 
         for(char c : t)
         {
-            if(hashMap.find(c)== hashMap.end() || hashMap[c] == 0)
-            {
-                return false;
-            }
-
             hashMap[c]--;
+        }
+
+        for(auto c : hashMap)
+        {
+            if(c.second<0) return false;
         }
 
         return true;
     }
 };
+
+// 만약 hashMap에 해당 key가 없는데 데이터를 사용하려고 하면 초기값으로 배정되고 생성된다
+// ++의 경우도 key가 없으면 0으로 초기화 된 후에 ++를 해주기에 key가 없다고 해서 대입이 아닌 연산이
+// 오류가 나는 것은 아니다
+
+// 그리고 돌다가 hashMap에 있는지 없는지 find로 찾는 것 보다, 그냥 value를 int로 두고, value값이 -이면 없는 것으로
+// 판단하는 것이 더 빠르다
