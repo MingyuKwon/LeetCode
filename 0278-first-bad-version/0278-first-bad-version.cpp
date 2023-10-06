faa59bbf4c0bebe5ebebfd7e7b55e6938f60661e
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-06 13:20:50 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (10.94%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,13 +4,13 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long left = 1;
-        long right = n;
-        long middle = (left + right) / 2;
+        int left = 1;
+        int right = n;
+        int middle = left + (right - left)/2;
 
         while(left <= right)
         {
-            middle = (left + right) / 2;
+            middle = left + (right - left)/2;
             if(isBadVersion(middle))
             {
                 right = middle-1;
@@ -23,5 +23,10 @@ public:
 
         return left;
     }
+// 여기서 중요한 것은 이진 탐색을 할 때 mid를 구하는 것이 (left + right) /2 가 아니라
+// mid = left + (right - left) / 2로 하는 것이다
+// 후자는 게산 결과 전자와 동일하게 되지만,
+// 전자는 left+ right에서 overflow가 일어날 수도 있지만 후자는 그럴 일이 없다
 
 };
+
