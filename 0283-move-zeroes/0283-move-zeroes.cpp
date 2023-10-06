2fc57e337f8db1188a3926560ad81a65d787d5bf
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-06 16:25:54 +0900

    Time: 26 ms (14.73%), Space: 19.7 MB (17.40%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int SIZE = nums.size();
-        int index = 0;
-        int zeroindex = SIZE;
-
-        while(index < zeroindex)
+        int j = 0;
+        for(int i=0; i<nums.size(); i++)
         {
-            if(nums[index] == 0)
+            if(nums[i] != 0)
             {
-                nums.push_back(0);
-                nums.erase(nums.begin() + index);
-                zeroindex--;
-                continue;
+                nums[j] = nums[i];
+                j++;
             }
+        }
 
-            index++;
+        for(;j<nums.size();j++)
+        {
+            nums[j] = 0;
         }
     }
+    //특정 조건만 만족하는 원소들만 배열에 남기기 혹은, 특정조건 만족하지 않는 원소들 뒤로 빼기라면
+    //이렇게 덮어쓰기용 포인터를 하나만들어서 덮어쓰는 방식이 매우 편리하다!
 };
+
