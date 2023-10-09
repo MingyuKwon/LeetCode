42e288430bab66f65fe2c6aeed3016ca2643bde1
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-09 12:08:14 +0900

    Time: 0 ms (100.00%), Space: 6.9 MB (74.45%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -3,18 +3,22 @@ public:
     char findTheDifference(string s, string t) {
         unordered_map<char, int> HashMap;
 
+        int sSum = 0;
+        int tSum = 0;
+
         for(char c : s)
         {
-            HashMap[c]++;
+            sSum += c;
         }
 
         for(char c : t)
         {
-            HashMap[c]--;
-
-            if(HashMap[c] < 0) return c;
+            tSum += c;
         }
 
-        return NULL;
+        return (char)(tSum - sSum);
     }
+    
+    // 단 하나만 차이가 나는 배열에서 그 다른 원소를 찾기 위해선
+    // 그냥 배열 원소를 다 더하고, 그 값을 서로 빼서 남은 값이 하나 다른 값이 나올 것이다
 };
