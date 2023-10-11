3e20c013a7cfc6bd35bcfcf5c85ab5db51aca7a4
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-11 14:45:03 +0900

    Time: 16 ms (56.49%), Space: 8.8 MB (52.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,20 +7,22 @@ public:
         int maxCount = 0;
         int subStringCount = 0;
         
-        for(int i=1; i<=stringCount; i++)
+        for(int i=0; i<stringCount; i++)
         {
-            if(HashMap[s[i-1]] + subStringCount < i) // 현재 substring에 겹치는 문자가 아님 
+            if(!HashMap.count(s[i]) || subStringCount + HashMap[s[i]] < i) // 현재 substring에 겹치는 문자가 아님 
             {
                 subStringCount++;
             }else
             {
-                subStringCount = i - HashMap[s[i-1]];
+                subStringCount = i - HashMap[s[i]];
             }
 
-            HashMap[s[i-1]] = i;
+            HashMap[s[i]] = i;
             maxCount = max(maxCount, subStringCount);
         }
 
         return maxCount;
     }
+
+    // count를 이용해서 map을 뒤지는 것이 가능, find 쓰지 말자
 };
