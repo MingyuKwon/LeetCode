e634544af2e7a789ec072c65956e55cb6deeb794
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-11 13:50:55 +0900

    Time: 25 ms (47.54%), Space: 71.9 MB (12.19%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,63 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* tail = dummyHead;
+        int carry = 0;
 
-        int upper = 0;
-        ListNode* temp = l1;
-        ListNode* head = temp;
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int digit1 = (l1 != nullptr) ? l1->val : 0;
+            int digit2 = (l2 != nullptr) ? l2->val : 0;
 
-        while(l1 || l2)
-        {
-            if(!l1)
-            {
-                int val = (l2->val + upper) % 10;
-                upper = (l2->val + upper) / 10;
-                temp->val = val;
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
 
-                l2 = l2->next;
-                temp->next = l2;
-            }else if(!l2)
-            {
-                int val = (l1->val + upper) % 10;
-                upper = (l1->val + upper) / 10;
-                temp->val = val;
+            ListNode* newNode = new ListNode(digit);
+            tail->next = newNode;
+            tail = tail->next;
 
-                l1 = l1->next;
-                temp->next = l1;
-            }else
-            {
-                int val = (l1->val + l2->val + upper) % 10;
-                upper = (l1->val + l2->val + upper) / 10;
-                temp->val = val;
-
-                l1 = l1->next;
-                l2 = l2->next;
-
-                if(l1){
-                    temp->next = l1;
-                }else
-                {
-                    temp->next = l2;
-                }
-            }
-            if(temp->next) temp = temp->next;
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
         }
 
-        if(upper > 0) 
-        {
-            temp->next = new ListNode(1);
-        }
-
-        return head;
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
     }
 };
