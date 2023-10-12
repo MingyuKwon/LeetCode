e0faa579eeec313c98272f072f95ecd4e48cf991
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-12 15:35:33 +0900

    Time: 6 ms (25.49%), Space: 6.9 MB (5.24%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+
+        while(s[i] == ' '){ // skipping space characters at the beginning
+            i++;
+        }
+
+        int positive = 0, negative = 0; 
+
+        if(s[i] == '+'){
+            positive++; // number of positive signs at the start in string
+            i++;
+        }
+
+        if(s[i] == '-'){
+            negative++; // number of negative signs at the start in string
+            i++;
+        }
+
+        double ans = 0; 
+
+        while(i<n and s[i] >= '0' and s[i] <= '9'){
+            ans = ans*10 + (s[i] - '0'); // (s[i] - '0') is converting character to integer
+            i++;
+        }
+
+        if(negative > 0) // if negative sign exist
+            ans = -ans;
+        if(positive > 0 and negative > 0) // if both +ve and -ve sign exist, Example: +-12
+            return 0;
+
+        if(ans > INT_MAX) // if ans > 2^31 - 1
+            ans = INT_MAX;
+
+        if(ans < INT_MIN) // if ans < 2^31
+            ans = INT_MIN;
+
+        return (int)ans;
+    }
+
+    // 앞에 0이 붙은 것을 지워야 하는 것은 늘 하던대로 num % 10 num /10 하다보면 
+    // 자연스레 무시하고 진행되게 되므로 괜찮다
+};
