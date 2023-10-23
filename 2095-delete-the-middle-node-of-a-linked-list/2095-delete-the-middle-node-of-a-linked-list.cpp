ec2b95ca007b0f2c401af625e754ef6050620edd
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-10-23 18:31:18 +0900

    Time: 579 ms (90.84%), Space: 295 MB (60.12%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -32,9 +32,4 @@ public:
 
         return head;
     }
-
-    // 그냥 index로 중간 값을 찾아도 좋지만, fast slow pointer를 사용하면 더 좋다
-    // fast slow pointer는 원래 사이클을 찾기 위한 방법이지만, 사이클이 없어도 특성 이용가능
-    // slow는 fast의 절반을 가므로, 최종적으로 slow는 직선 링크드 리스트에서 중간점에서 멈추게 될 것이다.  
-    // 따라서 slow에 뒤에 붙는 previous pointer 하나를 가지고 진행하면 된다
 };
