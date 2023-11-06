e76b6975bfede84a57e672cc46d441d99cccb4a6
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-11-06 14:05:49 +0900

    Time: 30 ms (61.16%), Space: 21.1 MB (58.50%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> HashMap;
-        vector<string> copyStrs = strs;
         vector<vector<string>> result;
 
-        for(string& str : copyStrs)
+        int i = 0;
+        for(string str : strs)
         {
             sort(str.begin(), str.end());
-        }
-
-        for(int i=0; i<copyStrs.size(); i++)
-        {
-            HashMap[copyStrs[i]].push_back(strs[i]);
+            HashMap[str].push_back(strs[i]);
+            i++;
         }
 
         for(auto a : HashMap)
@@ -22,4 +19,7 @@ public:
 
         return result;
     }
+
+    // 결국 아나그램을 부수는 것에는 정렬이 답인것 같다. 
+    // 아나그램해서 동일한 것은 정렬하면 모두 동일하게 나온다
 };
