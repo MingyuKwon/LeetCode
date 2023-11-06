3e9c8bb30c57aa206b5b7c573d17f6a6fbd83985
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2023-11-06 14:51:57 +0900

    Time: 78 ms (81.91%), Space: 70.8 MB (5.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,6 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         vector<int> DP(nums.size()+1, INT_MIN);
 
+        int MAX = INT_MIN;
         for(int i=1; i<=nums.size(); i++)
         {
             if(DP[i-1] < 0)
@@ -12,12 +13,8 @@ public:
             {
                 DP[i] = DP[i-1] + nums[i-1];
             }
-        }
 
-        int MAX = INT_MIN;
-        for(int i : DP)
-        {
-            if(MAX < i) MAX = i;
+            if(MAX < DP[i]) MAX = DP[i];
         }
 
         return MAX;
