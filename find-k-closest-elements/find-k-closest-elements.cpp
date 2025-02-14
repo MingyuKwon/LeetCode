020ef1f1b74acc5e5ae9b4feb0a2d74497b66cd2
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2025-02-14 14:06:02 +0900

    Time: 10 ms, Memory: 36.5 MB - LeetHub

--- find-k-closest-elements/find-k-closest-elements.cpp
+++ find-k-closest-elements/find-k-closest-elements.cpp
@@ -2,83 +2,66 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
-        
         vector<int> ans;
         
-        int left = 0;
-        int right = n-1;
-        
-        int xIndex = findX(arr, x);
-        if(xIndex < n && arr[xIndex] == x)
-        {
-            k--;
-            ans.push_back(x);
-            
-            left = xIndex - 1;
-            right = xIndex + 1;
-        }else
-        {
-            left = xIndex - 1;
-            right = xIndex;
-        }
-        
+        int right = findX(arr, x);
+        int left = right - 1;
+
         while(k--)
         {
-            if(left < 0)
+            if(left < 0) 
             {
                 ans.push_back(arr[right]);
                 right++;
-            }else if(right >= n)
+                continue;
+            }
+
+            if(right >= n) 
+            {
+                ans.push_back(arr[left]);
+                left--;
+                continue;
+            }
+
+            int leftGap = abs(arr[left]- x);
+            int rightGap = abs(arr[right]- x);
+
+            if(leftGap <= rightGap)
             {
                 ans.push_back(arr[left]);
                 left--;
             }else
             {
-                int leftGap = x - arr[left];
-                int rightGap = arr[right] - x;
-                
-                if(leftGap <= rightGap)
-                {
-                    ans.push_back(arr[left]);
-                    left--;
-                }else
-                {
-                    ans.push_back(arr[right]);
-                    right++;
-                }
+                ans.push_back(arr[right]);
+                right++;
             }
         }
-        
+
         sort(ans.begin(), ans.end());
-        return ans;
         
+        return ans;
     }
-    
+
     int findX(vector<int>& arr, int x)
     {
-        int n = arr.size();
-        
         int left = 0;
-        int right = n-1;
-        
+        int right = arr.size() - 1;
+
         while(left <= right)
         {
-            int mid = left + (right - left)/2;
-            
-            if(arr[mid] == x)
-            {
-                return mid;
-            }
-            
-            if(arr[mid] > x)
-            {
-                right = mid -1;
-            }else
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] < x)
             {
                 left = mid + 1;
+            }else
+            {
+                right = mid - 1;
             }
         }
-        
+
         return left;
     }
+    
+    
 };
