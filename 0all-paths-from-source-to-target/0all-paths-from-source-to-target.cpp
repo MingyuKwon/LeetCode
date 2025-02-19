a608e52c8a6769fa42ada5292e786d875b3171f5
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2025-02-19 23:31:06 +0900

    Time: 0 ms, Memory: 13.6 MB - LeetHub

--- 0all-paths-from-source-to-target/0all-paths-from-source-to-target.cpp
+++ 0all-paths-from-source-to-target/0all-paths-from-source-to-target.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> ans;
-
         vector<int> route;
         dfs(graph, ans, route, 0);
-
         return ans;
     }
 
     void dfs(vector<vector<int>>& graph, vector<vector<int>>& ans, vector<int>& route, int source)
     {
         int n = graph.size();
-
         route.push_back(source);
-
         if(source == n-1)
         {
             ans.push_back(route);
@@ -25,7 +21,6 @@ public:
                 dfs(graph, ans, route, node);
             }
         }
-
         route.pop_back();
     }
 };
