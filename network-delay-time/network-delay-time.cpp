fc5abd78a9cd7366f19ce684373c3a4617808a53
MingyuKwon <90129581+MingyuKwon@users.noreply.github.com>
2025-02-22 12:03:45 +0900

    Time: 76 ms, Memory: 48 MB - LeetHub

--- network-delay-time/network-delay-time.cpp
+++ network-delay-time/network-delay-time.cpp
@@ -11,29 +11,33 @@ public:
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; // weight, destination 
         pq.push({0, k});
 
-        vector<int> minTime(n+1, INT_MAX);
-        minTime[0] = 0;
-        
+        vector<int> minTime(n+1, INT_MAX);        
 
         while(!pq.empty())
         {
             auto[weight, destination] = pq.top();
             pq.pop();
 
-            if(minTime[destination] != INT_MAX) continue;
+            if(minTime[destination] < weight) continue;
+
             minTime[destination] = weight;
 
             for(pair<int, int> edge : graph[destination])
             {
-                pq.push({edge.second + weight, edge.first});
+                int newWeight = weight + edge.second;
+                if(minTime[edge.first] > newWeight) 
+                {
+                    minTime[edge.first] = newWeight;
+                    pq.push({newWeight, edge.first});
+                }
             }
 
         }
 
         int maxVal = 0;
-        for(int i : minTime)
+        for(int i =1; i<=n; i++)
         {
-            maxVal = max(maxVal, i);
+            maxVal = max(maxVal, minTime[i]);
         }
 
         return maxVal == INT_MAX ? -1 : maxVal;
